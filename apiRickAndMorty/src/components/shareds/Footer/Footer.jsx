c392af0d8cc1ac4132fb2d1c10f3b47e1f37b1fb
infousinacreativa-dev <info.usinacreativa@gmail.com>
2025-12-02 15:56:33 -0300

    cambios estilos

--- apiRickAndMorty/src/components/shareds/Footer/Footer.jsx
+++ apiRickAndMorty/src/components/shareds/Footer/Footer.jsx
@@ -1,10 +1,9 @@
 export function Footer() {
     return (
         <footer style={{ backgroundColor: '#503d2bff', color: '#d5d5d5', textAlign: 'center', margin: '0', padding: '1rem' }}>
-            <h1 >Practica de Consumo de API con servicios</h1>
-            <h2>Custom Hooks - Pagination - Filtros</h2>
-
-            <h2>:copy Senestrari Dev</h2>
+            <h2 >Practica de Consumo de API con servicios</h2>
+            <h3>Custom Hooks - Pagination - Filtros</h3>
+            <div style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'center', height:'50px', gap:'10px'}}><a href="https://senestraridev.com/"><img src="/img/logoSenestrariDev.webp" alt="logo de Senestrari Dev" style={{maxHeight:'35px'}} /></a></div>
         </footer>
     )
 }
--- apiRickAndMorty/src/components/shareds/Header/Header.jsx
+++ apiRickAndMorty/src/components/shareds/Header/Header.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router"
+
 export function Header() {
     return (
         <>
-            <nav style={{ backgroundColor: '#d5d5d5', color: '#000', textAlign: 'center',  }}>
-                <div>
-                    <h1 >Personajes de Rick And Morty</h1>
+            <nav style={{ backgroundColor: '#d5d5d5', color: '#000',padding: '1.5rem', display: 'flex', justifyContent:'center' }}>
+
+                <div style={{ width:'80%',display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }} >
+                    <h2 style={{color:'#422727', textTransform:'uppercase', textDecoration:'underline'}}>Explorador de Personajes de Rick y Morty</h2>
+
+                    <Link to={'https://rickandmortyapi.com/documentation'} style={{backgroundColor:'#422727', padding:'0.5rem', textDecoration:'none', color:'white', borderRadius:'16px'}}>Link a la Api</Link>
                 </div>
+
             </nav>
         </>
     )
